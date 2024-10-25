556025963a1c3ba5a97f331115afae87edf95d22
Daniel Jawna <djawna@gmail.com>
2024-10-26 00:09:06 +0200

    finish the editor implementation

--- App.cs
+++ App.cs
@@ -23,26 +23,76 @@ internal class App
 
         var menu = new MenuBar(new MenuBarItem[]{
             new MenuBarItem ("_File", new MenuItem []{
-                new MenuItem("_Open", "Open new resource file", () => {
-                    var filePicker = new FileDialog(title: "Open resource", prompt: "Confirm", message: "Pick an existing resource file", allowedTypes: new List<string>{"*.resx"});
+                new MenuItem("_New", "create new resource file", () => {
+                    var filePicker = new SaveDialog(title: "New resource", message: "Create a new file for the Resource", allowedTypes: new List<string>{".resx"});
                     Application.Run(filePicker);
                     if(!filePicker.Canceled) {
                         persistence.CurrentlySelectedResource = (string) filePicker.FilePath;
-                        mainWindow.SetCurrentFile((string)filePicker.FilePath);
+                        mainWindow.CurrentFile =(string)filePicker.FilePath;
+                        mainWindow.SetDataTable(new Dictionary<string, string>{
+                            {"example1", "Value one"},
+                        });
                     }
                     
                 }),
+                new MenuItem("_Open", "Open new resource file", () => {
+                    var filePicker = new OpenDialog(title: "Open resource", message: "Pick an existing resource file", allowedTypes: new List<string>{".resx"});
+                    Application.Run(filePicker);
+                    if(!filePicker.Canceled) {
+                        try {
+                            using var fs = new FileStream(path: (string) filePicker.FilePath, mode: FileMode.Open, access: FileAccess.Read);
+                            var resEditor = new ResXLib.ResourceEditor();
+                            mainWindow.SetDataTable(resEditor.ReadResxToDictionary(fs));
+                            mainWindow.CurrentFile =(string)filePicker.FilePath;
+                            persistence.CurrentlySelectedResource = (string) filePicker.FilePath;
+                        } catch(Exception ex) {
+                            MessageBox.ErrorQuery("Error",$"Error during loading of the Resource: {ex.Message}", "Ok");
+                        }
+                    }
+                }),
+                new MenuItem("_Save", "Save the current data", ()=> {
+                    if (mainWindow.Dirty) {
+                        SaveCurrentFile(mainWindow, persistence);
+                    }
+                }),
                 new MenuItem("_Quit", "Stop the app", () => {
-                    Application.RequestStop ();
+                    if (mainWindow.Dirty) {
+                        var choice = MessageBox.Query("Unsaved changes", "Do you want to save the current file?", "SaveAndQuit", "DiscardAndQuit", "Cancel");
+                        if(choice == 0) {
+                            SaveCurrentFile(mainWindow, persistence);
+                            if (!mainWindow.Dirty) {
+                                Application.RequestStop();
+                            }
+                        } else if (choice == 1) {
+                            Application.RequestStop();
+                        }
+                    } else {
+                        Application.RequestStop ();
+                    }
                 })
             })
         });
 
-        mainWindow.SetCurrentFile(persistence.CurrentlySelectedResource ?? "<None>");
+        mainWindow.CurrentFile =(persistence.CurrentlySelectedResource ?? "<None>");
 
         Application.Top.Add(menu, mainWindow);
         Application.Run();
 
         Application.Shutdown();
+    }
+
+
+    public static void SaveCurrentFile(MainWindow mainWindow, IPersistence persistence) {
+        try {
+            var currentData= mainWindow.GetFromDataTable();
+            var resEditor = new ResXLib.ResourceEditor();
+            using var fs = new FileStream(path: persistence.CurrentlySelectedResource, mode: FileMode.OpenOrCreate, access: FileAccess.Write);
+            var result = resEditor.WriteResxToDictionary(currentData);
+            result.CopyTo(fs);
+            fs.Close();
+            mainWindow.Dirty = false;
+        } catch (Exception ex) {
+            MessageBox.ErrorQuery("Error",$"Error during saving process: {ex.Message}", "Ok");
+        }
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -1,18 +1,56 @@
+using System.Data;
 using Terminal.Gui;
 
 namespace Resourcecreator;
 
 internal class MainWindow : View{
 
-    public void SetCurrentFile(string newValue) {
-        currentFileLabel.Text = newValue;
+    public string CurrentFile  
+    {   get {
+            return (string) currentFileLabel.Text;
+        } 
+        set {
+            currentFileLabel.Text = value;
+        }
+    }
+
+    private readonly Label currentFileLabel;
+    private readonly DataTable data;
+
+
+    public void SetDataTable(Dictionary<string,string> resourceData) {
+        foreach((var key, var value) in resourceData) {
+            var row = data.NewRow();
+            row["Key"] = key; 
+            row["Value"] = value;
+            data.Rows.Add(row);
+        }
+        placeHolder.Height = Dim.Percent(0f);
+        resourceTable.Height = Dim.Percent(100f);
+    }
+
+    public bool Dirty {get;set;}
+
+    public Dictionary<string,string> GetFromDataTable() {
+        var result = new Dictionary<string,string>();
+
+        foreach(DataRow row in data.Rows) {
+            string key = (string) row["Key"];
+            string value = (string) row["Value"];
+            result[key] = value;
+        }
+        return result;
 
     }
-    private readonly Label currentFileLabel;
+
+    private readonly Label placeHolder;
+    private readonly TableView resourceTable;
 
     public MainWindow () {
-        //Title = "Resource editor";
-
+        Dirty = false;
+        data = new DataTable();
+        data.Columns.Add(new DataColumn("Key"));
+        data.Columns.Add(new DataColumn("Value"));
         var currentFileView = new FrameView("CurrentFile:", new Border{ BorderStyle = BorderStyle.Single }){
             Width=Dim.Fill(),
             Height = Dim.Percent(10f)
@@ -21,16 +59,76 @@ internal class MainWindow : View{
         currentFileLabel = new Label(){
         };
         currentFileView.Add(currentFileLabel);
+
         var resourceEditView = new FrameView("ResourceContent:", new Border{ BorderStyle = BorderStyle.Single }){
             Y = Pos.Bottom(currentFileView),
             Width=Dim.Fill(),
             Height = Dim.Percent(90f)
         };
+        placeHolder= new Label(text: "No Data loaded!", autosize: true){
+            Width=Dim.Fill(),
+            Height = Dim.Percent(100f)
+        };
 
-        //var label = new Label("tltest");
+        resourceTable = new TableView(data){
+            Width=Dim.Fill(),
+            Height = Dim.Percent(0f)
+        };
 
-
+        resourceTable.CellActivated += EditCurrentCell;
+        resourceEditView.Add(placeHolder, resourceTable);
         Add(currentFileView, resourceEditView);
     }
 
+
+		private void EditCurrentCell (TableView.CellActivatedEventArgs e)
+		{
+			if (e.Table == null)
+				return;
+
+			var oldValue = e.Table.Rows [e.Row] [e.Col].ToString ();
+
+			if (GetText ("Enter new value", e.Table.Columns [e.Col].ColumnName, oldValue, out string newText)) {
+				try {
+					e.Table.Rows [e.Row] [e.Col] = string.IsNullOrWhiteSpace (newText) ? DBNull.Value : (object)newText;
+                    Dirty = true;
+				} catch (Exception ex) {
+					MessageBox.ErrorQuery (60, 20, "Failed to set text", ex.Message, "Ok");
+				}
+
+				resourceTable.Update ();
+			}
+		}
+    
+    		private bool GetText (string title, string label, string initialText, out string enteredText)
+		{
+			bool okPressed = false;
+
+			var ok = new Button ("Ok", is_default: true);
+			ok.Clicked += () => { okPressed = true; Application.RequestStop (); };
+			var cancel = new Button ("Cancel");
+			cancel.Clicked += () => { Application.RequestStop (); };
+			var d = new Dialog (title, 60, 20, ok, cancel);
+
+			var lbl = new Label () {
+				X = 0,
+				Y = 1,
+				Text = label
+			};
+
+			var tf = new TextField () {
+				Text = initialText,
+				X = 0,
+				Y = 2,
+				Width = Dim.Fill ()
+			};
+
+			d.Add (lbl, tf);
+			tf.SetFocus ();
+
+			Application.Run (d);
+
+			enteredText = okPressed ? tf.Text.ToString () : null;
+			return okPressed;
+		}
 }
